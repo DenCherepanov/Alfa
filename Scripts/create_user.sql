eecd0eb5f8869be1dd7338260a25486031af414f
Denis Cherepanov <santon1982@gmail.com>
2018-10-15 21:27:05 +0700

    Добавление скриптов для БД и исходных данных для загрузки

---
+++ Scripts/create_user.sql
@@ -0,0 +1,31 @@
+CREATE BIGFILE TABLESPACE tbs_01
+  DATAFILE 'tbs_01.dat'
+    SIZE 10M
+    AUTOEXTEND ON;
+
+CREATE USER tst
+  IDENTIFIED BY 123
+  DEFAULT TABLESPACE tbs_01;
+
+ALTER USER tst quota unlimited on tbs_01;
+
+GRANT create session TO tst;
+GRANT create table TO tst;
+GRANT create view TO tst;
+GRANT create trigger TO tst;
+GRANT create procedure TO tst;
+GRANT create sequence TO tst;
+GRANT create synonym TO tst;
+
+GRANT ALTER ANY table TO tst;
+GRANT ALTER ANY trigger TO tst;
+GRANT ALTER ANY procedure TO tst;
+
+GRANT DELETE ANY TABLE TO tst;
+GRANT DROP ANY TABLE TO tst;
+GRANT DROP ANY PROCEDURE TO tst;
+GRANT DROP ANY TRIGGER TO tst;
+GRANT DROP ANY VIEW TO tst;
+
+--drop tablespace tbs_01;
+--drop user tst cascade;
